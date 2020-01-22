f791ec46bd5521eb62a21b8461e62bd6240a7841
Thomas Lavend'Homme <lavendthomas@outlook.be>
2020-01-22 16:37:28 +0100

    Create a test UI with a button

--- src/window.py
+++ src/window.py
@@ -15,7 +15,8 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-from gi.repository import Gtk
+from gi.repository import Gtk, GLib
+from .vaults_handler import Vault
 
 
 @Gtk.Template(resource_path='/org/gnome/Safebox/window.ui')
