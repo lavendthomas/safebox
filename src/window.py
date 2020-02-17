0c4599f30201b11a87ac4d6ca8d1d5296275167f
Thomas Lavend'Homme <lavendthomas@outlook.be>
2020-02-17 21:20:10 +0100

    Add transitions between greeting screens

--- src/window.py
+++ src/window.py
@@ -35,22 +35,37 @@ class SafeboxWindow(Gtk.ApplicationWindow):
 
         self.handlers = {
             "onDestroy": Gtk.main_quit,
-            "on_mount_button_clicked": self._on_mount_button_clicked
+            "on_mount_button_clicked": self._on_mount_button_clicked,
+            "on_lets_get_started": self.on_lets_get_started,
+            "on_first_safebox_add_button_clicked": self.on_first_safebox_add_button_clicked
         }
-
-
 
         self.builder = Gtk.Builder()
         self.builder.add_from_resource(
             "/org/gnome/Safebox/window.ui")
         self.builder.connect_signals(self.handlers)
 
-        #self.button: Gtk.Button = self.builder.get_object("mount_button")
-
         window = self.builder.get_object("SafeboxWindow")
         window.show_all()
-        # self.connect("on_mount_button_clicked", self._test)
 
+    def on_lets_get_started(self, source):
+        first_safebox_stack: Gtk.Stack = self.builder.get_object("first_safebox_stack")
+        choice_box = self.builder.get_object("first_safebox_name_choice_box")
+        first_safebox_stack.set_visible_child(choice_box)
+
+    def on_first_safebox_add_button_clicked(self, source):
+
+        name_entry: Gtk.Entry = self.builder.get_object("first_safebox_name_entry")
+        pass_entry: Gtk.Entry = self.builder.get_object("first_safebox_password_entry")
+
+        name = name_entry.get_text()
+        password = pass_entry.get_text()
+
+        logging.info(name + " " + password)
+
+        first_safebox_stack: Gtk.Stack = self.builder.get_object("first_safebox_stack")
+        warning_box = self.builder.get_object("first_safebox_warning_box")
+        first_safebox_stack.set_visible_child(warning_box)
 
     def _on_mount_button_clicked(self, source):
         logging.info("Hello World!")
